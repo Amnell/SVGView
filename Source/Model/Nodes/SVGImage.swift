bb4f1f24865edc7bb21cff970404067b2c2c8469
Mathias Amnell <104110+Amnell@users.noreply.github.com>
2026-05-25 16:16:34 +0200

    Implement coords-viewattr-02-b image preserveAspectRatio rendering
    Decode embedded JPEG data URIs for <image> elements and apply preserveAspectRatio layout in SwiftUI image views. Add SVG 1.1 coords-viewattr-02-b test coverage, generated reference, CLI ref list, and coverage table update.

--- Source/Model/Nodes/SVGImage.swift
+++ Source/Model/Nodes/SVGImage.swift
@@ -19,22 +19,38 @@ public class SVGImage: SVGNode {
     public var y: CGFloat
     public var width: CGFloat
     public var height: CGFloat
+    public var preserveAspectRatio: SVGPreserveAspectRatio
 #else
     @Published public var x: CGFloat
     @Published public var y: CGFloat
     @Published public var width: CGFloat
     @Published public var height: CGFloat
+    @Published public var preserveAspectRatio: SVGPreserveAspectRatio
 #endif
 
-    public init(x: CGFloat = 0, y: CGFloat = 0, width: CGFloat = 0, height: CGFloat = 0) {
+    public init(
+        x: CGFloat = 0,
+        y: CGFloat = 0,
+        width: CGFloat = 0,
+        height: CGFloat = 0,
+        preserveAspectRatio: SVGPreserveAspectRatio = SVGPreserveAspectRatio()
+    ) {
         self.x = x
         self.y = y
         self.width = width
         self.height = height
+        self.preserveAspectRatio = preserveAspectRatio
     }
 
     override func serialize(_ serializer: Serializer) {
-        serializer.add("x", x, 0).add("y", y, 0).add("width", width, 0).add("height", height, 0)
+        serializer
+            .add("x", x, 0)
+            .add("y", y, 0)
+            .add("width", width, 0)
+            .add("height", height, 0)
+            .add("scaling", preserveAspectRatio.scaling)
+            .add("xAlign", preserveAspectRatio.xAlign)
+            .add("yAlign", preserveAspectRatio.yAlign)
         super.serialize(serializer)
     }
 }
